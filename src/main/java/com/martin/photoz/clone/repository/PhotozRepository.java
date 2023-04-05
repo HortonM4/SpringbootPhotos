96a88b0d2bfc978944402d3016903a1e3ecec6ce
martinhorton <martinhorton7@gmail.com>
2023-04-05 18:56:56 +0100

    commit

---
+++ src/main/java/com/martin/photoz/clone/repository/PhotozRepository.java
@@ -0,0 +1,8 @@
+package com.martin.photoz.clone.repository;
+
+import com.martin.photoz.clone.model.Photo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PhotozRepository extends CrudRepository<Photo, Integer> {
+
+}
---
+++ src/main/java/com/martin/photoz/clone/service/PhotozService.java
@@ -0,0 +1,43 @@
+package com.martin.photoz.clone.service;
+
+import com.martin.photoz.clone.model.Photo;
+import com.martin.photoz.clone.repository.PhotozRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+
+@Service
+public class PhotozService {
+
+    private final PhotozRepository photozRepository;
+
+
+    public PhotozService(PhotozRepository photozRepository) {
+        this.photozRepository = photozRepository;
+    }
+
+    public Iterable<Photo> get() {
+        return photozRepository.findAll();
+    }
+
+    public Photo get(Integer id) {
+        return photozRepository.findById(id).orElse(null);
+    }
+
+    public void remove(Integer id) {
+        photozRepository.deleteById(id);
+    }
+
+    public Photo save(String fileName, String contentType, byte[] data) {
+        Photo photo = new Photo();
+        photo.setContentType(contentType);
+
+        photo.setFileName(fileName);
+        photo.setData(data);
+        photozRepository.save(photo);
+        return photo;
+    }
+}
